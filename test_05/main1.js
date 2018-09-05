af0f8c83ddaf70f9db808cbdd6f1a960f92d3534
sulingLiang <1240956406@qq.com>
2018-09-05 14:12:50 +0800

    test_05~09

---
+++ test_05/main1.js
@@ -0,0 +1,7 @@
+let imgSmall = document.createElement("img");
+imgSmall.src = require("./small.jpg");
+document.body.appendChild(imgSmall);
+
+let imgBig = document.createElement("img");
+imgBig.src = require("./big.jpg");
+document.body.appendChild(imgBig);
