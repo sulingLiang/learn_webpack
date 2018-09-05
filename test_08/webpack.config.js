c929ebdffb6e96d48ee87cfe933726b6259b49df
sulingLiang <1240956406@qq.com>
2018-09-05 14:19:08 +0800

    recommit test_08

---
+++ test_08/webpack.config.js
@@ -0,0 +1,18 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var OpenBrowserPlugin = require("open-browser-webpack-plugin");
+
+module.exports = {
+	entry:"./main.js",
+	output:{
+		filename:"bundle.js"
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			title:"webpack-test_08",
+			filename:"index.html"
+		}),
+		new OpenBrowserPlugin({
+			url:"http://localhost:8080"
+		})
+	]
+}
